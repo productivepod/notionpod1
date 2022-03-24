018f90f5cd64bdfa3f368565ec07741c7bae0776
Productive Pod <theproductivepod@gmail.com>
2022-03-24 14:54:00 +0530

    1.3 using notionpod for notion blogs. working fine. Next-Pagination in Notion

--- pages/blog.js
+++ pages/blog.js
@@ -1,46 +1,18 @@
-import { Client } from '@notionhq/client'
 import siteMetadata from '@/data/siteMetadata'
 import ListLayout from '@/layouts/ListLayout'
 import { PageSEO } from '@/components/SEO'
+import { getAllPostsNotion } from '@/lib/notionpod'
+export const POSTS_PER_PAGE = 5
 
 export const getStaticProps = async () => {
-  const notion = new Client({
-    auth: process.env.NOTION_SECRET,
-  })
-
-  const data = await notion.databases.query({
-    database_id: process.env.NOTION_DB_ID,
-    filter: {
-      property: 'Published',
-      checkbox: {
-        equals: true,
-      },
-    },
-  })
-
-  const notiondata = data.results.map((post) => ({
-    id: post.id,
-    date: post.created_time,
-    lastmod: post.last_edited_time,
-    title: post.properties.Name.title[0].plain_text,
-    tags: post.properties.Tags.multi_select.map((tag) => tag.name),
-    slug: post.properties.Slug.formula.string,
-    draft: post.properties.Published.checkbox,
-    summary: 'abcd',
-    layout: 'PostSimple',
-  }))
-
+  const notiondata = await getAllPostsNotion(process.env.NOTION_DB_ID)
   const initialDisplayPosts = notiondata.slice(0, POSTS_PER_PAGE)
-
   const pagination = {
     currentPage: 1,
     totalPages: Math.ceil(notiondata.length / POSTS_PER_PAGE),
   }
-
   return { props: { initialDisplayPosts, posts: notiondata, pagination } }
 }
-
-export const POSTS_PER_PAGE = 5
 
 export default function Blog({ posts, initialDisplayPosts, pagination }) {
   //console.log(posts);
